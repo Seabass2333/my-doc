ea14d74160ab0d108ffb34c045b113d481b7a7a6
luhan <luhan@360.cn>
2024-12-10 14:56:39 +0800

    feat: add shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-center p-24'>
-      <h1 className='text-4xl font-bold'>Hello World</h1>
+      <Button variant='destructive'>Click me</Button>
     </main>
   )
 }
