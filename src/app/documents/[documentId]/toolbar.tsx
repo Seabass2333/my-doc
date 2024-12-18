dca83b9fbc61c5132f17afc72e71a88cc1497329
Seabass2333 <luhan.extra@gmail.com>
2024-12-18 18:06:07 +0800

    feat: add list

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -17,6 +17,7 @@ import {
   ItalicIcon,
   Link2Icon,
   ListIcon,
+  ListOrderedIcon,
   ListTodoIcon,
   LucideIcon,
   MessageCircleIcon,
@@ -111,6 +112,7 @@ const ListButton = () => {
 // AlignButton
 const AlignButton = () => {
   const { editor } = useEditorStore()
+
   const lists = [
     {
       label: 'Bullet List',
@@ -118,28 +120,33 @@ const AlignButton = () => {
       isActive: () => editor?.isActive('bulletList'),
       onClick: () => editor?.chain().focus().toggleBulletList().run()
     },
-    {}
+    {
+      label: 'Ordered List',
+      icon: ListOrderedIcon,
+      isActive: () => editor?.isActive('orderedList'),
+      onClick: () => editor?.chain().focus().toggleOrderedList().run()
+    }
   ]
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <button className='min-w-7 h-7 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm'>
-          <AlignLeftIcon className='size-4' />
+          <ListIcon className='size-4' />
         </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className='p-1 flex flex-col gap-y-1'>
-        {alignments.map(({ label, value, icon: Icon }) => (
+        {lists.map(({ label, isActive, onClick, icon: Icon }) => (
           <button
-            key={value}
-            onClick={() => editor?.chain().focus().setTextAlign(value).run()}
+            key={label}
+            onClick={onClick}
             className={cn(
               'flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80',
-              editor?.getAttributes('textStyle')?.textAlign === value &&
-                'bg-neutral-200/80'
+              isActive() && 'bg-neutral-200/80'
             )}
           >
             <Icon className='size-4' />
-            {label}
+            <span className='text-sm'>{label}</span>
           </button>
         ))}
       </DropdownMenuContent>
