a46de1b15848c329608e410eb239c16f88c1f013
Seabass2333 <luhan.extra@gmail.com>
2024-12-24 18:17:02 +0800

    feat: change schema data

--- convex/schema.ts
+++ convex/schema.ts
@@ -4,8 +4,5 @@ import { v } from "convex/values";
 export default defineSchema({
   documents: defineTable({
     title: v.string(),
-    content: v.string(),
-    createdAt: v.number(),
-    updatedAt: v.number(),
   }),
 });
