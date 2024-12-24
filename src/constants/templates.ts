c3d3728f302099890982478a4b9077f6d8aaa998
Seabass2333 <luhan.extra@gmail.com>
2024-12-24 13:57:32 +0800

    feat: add templates

---
+++ src/constants/templates.ts
@@ -0,0 +1,37 @@
+export const templates = [
+  {
+    id: 'blank',
+    title: 'Blank',
+    image: '/blank-document.svg'
+  },
+  {
+    id: 'software-proposal',
+    title: 'Software Proposal',
+    image: '/software-proposal.svg'
+  },
+  {
+    id: 'project-proposal',
+    title: 'Project Proposal',
+    image: '/project-proposal.svg'
+  },
+  {
+    id: 'business-letter',
+    title: 'Business Letter',
+    image: '/business-letter.svg'
+  },
+  {
+    id: 'resume',
+    title: 'Resume',
+    image: '/resume.svg'
+  },
+  {
+    id: 'cover-letter',
+    title: 'Cover Letter',
+    image: '/cover-letter.svg'
+  },
+  {
+    id: 'letter',
+    title: 'Letter',
+    image: '/letter.svg'
+  }
+]
