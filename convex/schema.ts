02f7e0bd47717f4fffab63c70eb728852aecec90
Seabass2333 <luhan.extra@gmail.com>
2024-12-24 18:13:37 +0800

    feat: add schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -4,5 +4,8 @@ import { v } from "convex/values";
 export default defineSchema({
   documents: defineTable({
     title: v.string(),
+    content: v.string(),
+    createdAt: v.number(),
+    updatedAt: v.number(),
   }),
 });
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,21 @@
+'use client'
+
+import { useQuery } from 'convex/react'
 import Navbar from './navbar'
 import TemplatesGallery from './templates-gallery'
+import { api } from '../../../convex/_generated/api'
 
 export default function Home() {
+  const documents = useQuery(api.documents.getDocuments)
+
+  if (!documents) {
+    return (
+      <div className='flex justify-center items-center h-screen'>
+        Loading...
+      </div>
+    )
+  }
+
   return (
     <div className='min-h-screen flex flex-col'>
       <div className='sticky top-0 left-0 z-50 h-16 bg-white p-4'>
@@ -9,6 +23,10 @@ export default function Home() {
       </div>
       <div className='mt-16'>
         <TemplatesGallery />
+        {documents &&
+          documents.map((document) => (
+            <div key={document._id}>{document.title}</div>
+          ))}
       </div>
     </div>
   )
